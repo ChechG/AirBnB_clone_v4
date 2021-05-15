ef153a131aaadb39d11413ac5f42bb91a25558c3
Tadeo Grach <tade.g.17@gmail.com>
2021-05-15 21:04:20 +0000

    fixing files to be semistandard compilant

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,13 +1,13 @@
-$(document).ready(function() {
-    $('.chckbx').click(function() {
-      let text= "";
-      let ids = [];
-      $('.chckbx:checked').each(function() {
-        text+=' ' + $(this).attr('data_name')+ ',';
-        ids.push($(this).attr('data_id'));
-      });
-      text=text.substring(0, text.length-1);
-      $("#selectedtext").text(text);
-      $('#count').val(ids);
+$(document).ready(function () {
+  $('.chckbx').click(function () {
+    let text = '';
+    const ids = [];
+    $('.chckbx:checked').each(function () {
+      text += ' ' + $(this).attr('data_name') + ',';
+      ids.push($(this).attr('data_id'));
     });
+    text = text.substring(0, text.length - 1);
+    $('#selectedtext').text(text);
+    $('#count').val(ids);
   });
+});
